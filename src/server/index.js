283fcd94298f9f4f73ae51f07ca8e71e3478b813
Nicolae Cojuhari <nicojuhari@gmail.com>
2019-10-04 13:32:19 +0100

    installed Aylien SDK and API keys

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
-
-
+const dotenv = require('dotenv'); 
+      dotenv.config();
+var AYLIENTextAPI = require('aylien_textapi');
+      
+//declare variables
 const port = 8080;
 const app = express();
+var nlp = new AYLIENTextAPI({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
 
 app.use(express.static('dist'));
 
@@ -15,4 +21,18 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.sendFile('dist/index.html')
+});
+
+app.get('/apiCall', (req, res) => {
+    
+    nlp.sentiment({
+        'text': 'John is a very good football player!'
+      }, function(error, response) {
+        if (error === null) {
+          console.log(response);
+          res.send(response);
+        }
+      });
+
+
 });

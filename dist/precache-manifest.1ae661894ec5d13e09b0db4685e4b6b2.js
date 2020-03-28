81c795865678c92fc84dbd83dd3f50df3f89c0f5
Nicolae Cojuhari <nicojuhari@gmail.com>
2020-03-28 16:42:37 +0000

    adds dist folder to the project

---
+++ dist/precache-manifest.1ae661894ec5d13e09b0db4685e4b6b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5476dbbac856a8e5869a31cd8c16c258",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97c47e26adc5dad983f0",
+    "url": "main.css"
+  },
+  {
+    "revision": "97c47e26adc5dad983f0",
+    "url": "main.js"
+  },
+  {
+    "revision": "1059f5650a5eb1b31e78a60c7ae7e40c",
+    "url": "public/bg-2.jpg"
+  },
+  {
+    "revision": "e2f51e5880990065574541c7439c0348",
+    "url": "public/fb.svg"
+  },
+  {
+    "revision": "1adfad51cfa5d865b8a2fbc5a214e679",
+    "url": "public/gh.svg"
+  },
+  {
+    "revision": "825699d6d0b4bcfbdc8e730f1b4263ad",
+    "url": "public/in.svg"
+  },
+  {
+    "revision": "e5ed5c6288d5d294a607ac99a13d8b02",
+    "url": "public/loading.gif"
+  },
+  {
+    "revision": "69a7ed81dcc527cbc7eb7f5701aa602a",
+    "url": "public/logo.svg"
+  },
+  {
+    "revision": "dc2b04a763665ed0f728c83720e532e1",
+    "url": "public/search.svg"
+  }
+]);
